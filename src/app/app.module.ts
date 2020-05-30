c255647cff0c89036785519f21a1ad1a53fcff6a
Qarun-Qadir-Bissoondial <qarun80@hotmail.com>
2020-05-29 20:42:56 -0400

    Added counter component and references to app compoentn

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CounterComponent } from './counter/counter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterComponent,
   ],
   imports: [
     BrowserModule,
